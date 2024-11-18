9c86a27292a7e43ea7c507c6de9b80eaf81e2156
cbuzzz <sebpinsan12@gmail.com>
2024-11-18 15:58:40 +0100

    First commit

--- src/routes/experiencias.ts
+++ src/routes/experiencias.ts
@@ -5,44 +5,55 @@ import * as experienciasServices from '../services/experienciasServices'
 
 const router = express.Router()
 
-router.get('/', async(_req, res) => {
+router.get('/', async (_req, res) => {
     const data = await experienciasServices.getEntries.getAll()
     return res.json(data);
 })
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     const data = await experienciasServices.getEntries.findById(req.params.id)
     return res.json(data);
 })
 
-router.get('/user/:id', async(req, res) => {
+router.get('/user/:id', async (req, res) => {
     const data = await experienciasServices.getEntries.findUserById(req.params.id)
     return res.json(data);
 })
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     const data = await experienciasServices.getEntries.create(req.body)
     return res.json(data);
 })
 
-router.post('/addParticipant/:idExp/:idPart', async(req, res) => {
-    const data = await experienciasServices.getEntries.addParticipant(req.params.idExp,req.params.idPart)
+router.post('/addParticipant/:idExp/:idPart', async (req, res) => {
+    const data = await experienciasServices.getEntries.addParticipant(req.params.idExp, req.params.idPart)
     return res.json(data);
 })
 
-router.put('/:id', async(req, res) => {
-    const data = await experienciasServices.getEntries.update(req.params.id,req.body)
+router.put('/:id', async (req, res) => {
+    const data = await experienciasServices.getEntries.update(req.params.id, req.body)
     return res.json(data);
 })
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
     const data = await experienciasServices.getEntries.delete(req.params.id)
     return res.json(data);
 })
 
-router.delete('/delParticipant/:idExp/:idPart', async(req, res) => {
-    const data = await experienciasServices.getEntries.delParticipant(req.params.idExp,req.params.idPart)
+router.delete('/delParticipant/:idExp/:idPart', async (req, res) => {
+    const data = await experienciasServices.getEntries.delParticipant(req.params.idExp, req.params.idPart)
     return res.json(data);
 })
+
+router.get('/search', async (req, res) => {
+    const { username } = req.query;
+
+    try {
+        const experiencias = await experienciasServices.getEntries.getExperiencesByUsername(username as string);
+        res.json(experiencias);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching experiences', error });
+    }
+});
 
 export default router
--- src/services/experienciasServices.ts
+++ src/services/experienciasServices.ts
@@ -1,34 +1,39 @@
 import { experienciasofDB } from "../modelos/types_d_experiencias";
 
 export const getEntries = {
-    getAll: async()=>{
-    return await experienciasofDB.find();
+    getAll: async () => {
+        return await experienciasofDB.find();
     },
-    findById: async(id:string)=>{
+    findById: async (id: string) => {
         return await experienciasofDB.findById(id);
     },
-    findUserById: async(id:string)=>{
+    findUserById: async (id: string) => {
         return await experienciasofDB.findById(id).populate('owner').populate('participants');
     },
-    addParticipant: async(idExp:string,idPart:string)=>{
-        return await experienciasofDB.findByIdAndUpdate(idExp,{$addToSet:{participants:idPart}});
+    addParticipant: async (idExp: string, idPart: string) => {
+        return await experienciasofDB.findByIdAndUpdate(idExp, { $addToSet: { participants: idPart } });
     },
     delParticipant: async (idExp: string, idPart: string) => {
         return await experienciasofDB.findByIdAndUpdate(
-          idExp,
-          { $pull: { participants: idPart } }, // Usamos $pull para eliminar solo el participante
-          { new: true } // Opción para devolver el documento actualizado
+            idExp,
+            { $pull: { participants: idPart } }, // Usamos $pull para eliminar solo el participante
+            { new: true } // Opción para devolver el documento actualizado
         );
-      }
+    }
     ,
-    create: async(entry:object)=>{
+    create: async (entry: object) => {
         return await experienciasofDB.create(entry);
     },
-    update: async(id:string,body:object)=>{
+    update: async (id: string, body: object) => {
         console.log(body);
-        return await experienciasofDB.findByIdAndUpdate(id,body,{$new:true});
+        return await experienciasofDB.findByIdAndUpdate(id, body, { $new: true });
     },
-    delete: async(id:string)=>{
+    delete: async (id: string) => {
         return await experienciasofDB.findByIdAndDelete(id);
+    },
+    getExperiencesByUsername: async (username: string) => {
+        return await experienciasofDB.find({
+            $or: [{ owner: username }, { participants: username }],
+        });
     }
 }
